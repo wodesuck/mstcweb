26a215a5a74e09a277c062ef9fda8d8ffc053613
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2013-08-21 15:07:25 +0800

    form model framework

--- models/form.py
+++ models/form.py
@@ -1,4 +1,141 @@
-class Form:
+import json, datetime
+from common.db import connect_db
 
-    def __init__(self):
+__all__ = ['Form', 'FormData', 'FieldDescription',
+'InvalidSubmit', 'NameExisted', 'NoSuchForm', 'NoSuchName',
+'NotStartYet', 'Ended']
+
+class Form(object):
+
+    def __init__(self, name = None):
+        """
+        Initialize a model object by the given event name.
+        Create an empty model object if `name` is omitted or None.
+        
+        Raises `NoSuchName` if there doesn't exist such an event.
+        """
+        if name is not None:
+            self.conn = connect_db()
+            self.cursor = self.conn.cursor()
+
+            if not self.cursor.execute(
+                    """SELECT `id`, `name`, `content_fields`, `start_time`,
+                    `end_time`, `created_time` FROM `events`
+                    WHERE `name` = %s""", name):
+                raise NoSuchForm
+
+            (self.id, self.name, self.content_fields, self.start_time,
+                    self.end_time, self.created_time) = self.cursor.fetchone()
+            self.content_fields = map(
+                    lambda x: FieldDescription(*x),
+                    json.loads(self.content_fields))
+
+    def submit(self, name, email, content):
+        """
+        Submit an application form to the event.
+
+        Raises `InvalidSubmit` if the submited data are invalid
+        or the model object is not initialized as an existing
+        event.
+        Raises `NotStartYet` if the event has not started yet.
+        Raises `Ended` if the event has ended already.
+
+        Returns the id of the new application form.
+        """
         pass
+
+    def query(self, items_per_page = 0, page = 0, status = None):
+        """
+        Query the application forms of the event.
+        Data are filtered by `status` if it is not None.
+
+        Returns a list containing the application forms.
+        Each form is presented as an instance of `FormData`.
+        """
+        pass
+
+    @classmethod
+    def query_one(cls, form_id):
+        """
+        Query one application forms by the form id.
+        *This is a class method.*
+
+        Raises `NoSuchForm` if there doesn't exist such an application form.
+        """
+        pass
+
+    @classmethod
+    def create_event(cls, name, content_fields, start_time, end_time):
+        """
+        Create an event and create an model object for the
+        new event.
+        *This is a class method.*
+
+        Raises `NameExisted` if the name has already existed.
+
+        `start_time` and `end_time` are `datetime.datetime` objects.
+        """
+        pass
+
+    @classmethod
+    def delete_event(cls, event_id = None, name = None):
+        """
+        Delete an event with the given event id or name.
+        Either `event_id` or `name` must be specified.
+        *This is a class method.*
+        """
+        pass
+
+    @classmethod
+    def change_form_status(cls, form_id, new_status):
+        """
+        Change the status of a specified application form.
+
+        Raises `NoSuchForm` if there doesn't exist such an application form.
+        *This is a class method.*
+        """
+        pass
+
+
+class FormData(object):
+    def __init__(self,
+            form_id, event_id, name, content, status, created_time):
+        pass
+
+
+class FieldDescription(object):
+    """
+    A class for defining a data field of a form content.
+
+    The argument `field_type` can be
+    'input': short text with the length between min_len and max_len
+    'textarea': long text with the length between min_len and max_len
+    'number': number between min_val and max_val
+    'bool': boolean value.
+    """
+    def __init__(self, column_name, field_type = 'input',
+            min_len = 0, max_len = 65536,
+            min_val = -65535, max_val = 65535):
+        pass
+
+    def to_json(self):
+        pass
+
+
+class NoSuchForm(Exception):
+    pass
+
+class NoSuchName(Exception):
+    pass
+
+class NameExisted(Exception):
+    pass
+
+class InvalidSubmit(Exception):
+    pass
+
+class NotStartYet(Exception):
+    pass
+
+class Ended(Exception):
+    pass
