ddb32595e83e4020a162e0995b9ad69e9321d079
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2013-08-22 12:58:05 +0800

    implement form model's query functions

--- models/form.py
+++ models/form.py
@@ -1,4 +1,5 @@
 import json, datetime
+import MySQLdb
 from common.db import connect_db
 
 __all__ = ['Form', 'FormData', 'FieldDescription',
@@ -27,7 +28,7 @@ class Form(object):
             (self.id, self.name, self.content_fields, self.start_time,
                     self.end_time, self.created_time) = self.cursor.fetchone()
             self.content_fields = map(
-                    lambda x: FieldDescription(*x),
+                    lambda x: FieldDescription(**x),
                     json.loads(self.content_fields))
 
     def submit(self, name, email, content):
@@ -52,7 +53,16 @@ class Form(object):
         Returns a list containing the application forms.
         Each form is presented as an instance of `FormData`.
         """
-        pass
+        sql = """SELECT `id`, `event_id`,
+        `name`, `email`, `content`, `status`, `created_time`
+        FROM `forms_data`"""
+        if status is not None:
+            sql += " WHERE `status` = %s" % MySQLdb.string_literal(status)
+        if items_per_page:
+            sql += ' LIMIT %d, %d' % (items_per_page * page, items_per_page)
+
+        self.cursor.execute(sql)
+        return map(lambda row: FormData(*row), self.cursor.fetchall())
 
     @classmethod
     def query_one(cls, form_id):
@@ -62,7 +72,16 @@ class Form(object):
 
         Raises `NoSuchForm` if there doesn't exist such an application form.
         """
-        pass
+        conn = connect_db()
+        cursor = conn.cursor()
+        sql = """SELECT `id`, `event_id`,
+        `name`, `email`, `content`, `status`, `created_time`
+        FROM `forms_data` WHERE `id` = %s"""
+
+        if not cursor.execute(sql, form_id):
+            raise NoSuchForm
+
+        return FormData(*cursor.fetchone())
 
     @classmethod
     def create_event(cls, name, content_fields, start_time, end_time):
@@ -99,13 +118,17 @@ class Form(object):
 
 class FormData(object):
     def __init__(self,
-            form_id, event_id, name, content, status, created_time):
-		self.form_id = form_id
-		self.event_id = event_id
-		self.name = name
-		self.content = content
-		self.status = status
-		self.created_time = created_time
+            form_id, event_id, name, email, content, status, created_time):
+        self.form_id = form_id
+        self.event_id = event_id
+        self.name = name
+        self.email = email
+        if isinstance(content, str):
+            self.content = json.loads(content)
+        else:
+            self.content = content
+        self.status = status
+        self.created_time = created_time
 
 
 class FieldDescription(object):
