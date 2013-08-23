13f93cc78d5ff7794ac640f8aefd8bfda8488099
wodesuck <wodesuck@gmail.com>
2013-08-23 13:08:41 +0800

    Change the constructor of Page. Use Page.get(name) to fetch page from db instead.

--- models/page.py
+++ models/page.py
@@ -10,18 +10,25 @@ class Page(object):
     prop = ['id', 'name', 'title', 'content', 'layout',
             'created_time', 'updated_time']
 
-    def __init__(self, name=None):
+    def __init__(self, **kwargs):
+        """
+        Initialize a Page object.
+        Assign object attribute by kwargs
+        """
+        self.__dict__ = kwargs
+
+    @classmethod
+    def get(cls, name):
         """
         Initialize a Page object by given name.
         Create an empty object if `name` is omitted or None.
 
         Raises `NoSuchPage` if such page doesn't exist.
         """
-        if name is not None:
-            sql = "SELECT %s FROM pages WHERE name = %%s" % ','.join(self.prop)
-            if not self.cursor.execute(sql, name):
-                raise NoSuchPage
-            self.__dict__ = dict(zip(self.prop, self.cursor.fetchone()))
+        sql = "SELECT %s FROM pages WHERE name = %%s" % ','.join(cls.prop)
+        if not cls.cursor.execute(sql, name):
+            raise NoSuchPage
+        return cls(**dict(zip(cls.prop, cls.cursor.fetchone())))
 
 
 class NoSuchPage(Exception):
