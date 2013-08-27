58403cfc9a3f6f77d8c0d072384e95dfa46ed727
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2013-08-27 14:15:43 +0800

    finish /forms/<name>

--- routes/forms.py
+++ routes/forms.py
@@ -1,9 +1,35 @@
+# -*- coding: utf8 -*-
 from routes import app
 from models import form
+import json
+
+from flask import (request, jsonify, render_template, abort)
 
 @app.route('/forms/<name>', methods = ['GET', 'POST'])
 def forms(name):
-    pass
+    try:
+        eventObj = form.Event.get(name)
+    except form.NoSuchEvent:
+        abort(404)
+
+    if request.method == 'GET':
+        return render_template('form.html', **eventObj.__dict__)
+    else:
+        try:
+            print request.form['content']
+            form_id = eventObj.submit(
+                    request.form['name'],
+                    request.form['email'],
+                    json.loads(request.form['content']))
+        except form.InvalidSubmit:
+            return jsonify(err_code = -1, msg = u'提交内容无效')
+        except form.NotStartYet:
+            return jsonify(err_code = -2, msg = u'该报名时间尚未开始')
+        except form.Ended:
+            return jsonify(err_code = -3, msg = u'该报名时间已结束')
+        else:
+            return jsonify(err_code = 0,
+                    msg = u'报名成功！报名编号：%d' % form_id)
 
 @app.route('/admin/forms/new')
 def admin_forms_new():
@@ -17,7 +43,7 @@ def admin_forms_delete(name):
 def admin_forms_edit(name):
     pass
 
-@app.route('/admin/forms/save', methods = ['PATCH', 'POST'])
+@app.route('/admin/forms', methods = ['PATCH', 'POST'])
 def admin_forms_save():
     pass
 
@@ -29,6 +55,6 @@ def admin_forms_query(name):
 def admin_forms_query_by_id(form_id):
     pass
 
-@app.route('/admin/forms/<int:form_id>/status/<int:status>', methods ['POST'])
+@app.route('/admin/forms/<int:form_id>/status/<int:status>', methods = ['POST'])
 def admin_forms_change_status(form_id, status):
     pass
