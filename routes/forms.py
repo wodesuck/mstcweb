818c9449285489dc5b1c457fb2fc1ef11ab32e2f
CJK-Regan <regan.cjk@gmail.com>
2013-08-27 23:03:55 +0800

    finish 3, 5

--- routes/forms.py
+++ routes/forms.py
@@ -37,20 +37,55 @@ def admin_forms_new():
 
 @app.route('/admin/forms/<name>', methods = ['DELETE'])
 def admin_forms_delete(name):
-    pass
+    # I think this is unnecessary
+    try:
+        eventObj = form.Event.get(name)
+    except form.NoSuchEvent:
+        return jsonify(err_code = -1, msg = u'报名事件（%s）不存在' % name)
+    #
+    form.Event.delete_event(name = name)
+    return jsonify(err_code = 0, msg = u'报名事件（%s）已删除' % name)
 
 @app.route('/admin/forms/<name>/edit')
 def admin_forms_edit(name):
     try:
         eventObj = form.Event.get(name)
     except form.NoSuchEvent:
-		abort(404)
+        abort(404)
 
     return render_template('event_edit.html', **eventObj.__dict__)
 
 @app.route('/admin/forms', methods = ['PATCH', 'POST'])
 def admin_forms_save():
-    pass
+    if request.method == 'PATCH':
+        try:
+            eventObj = form.Event.get(name)
+        except form.NoSuchEvent:
+            return jsonify(err_code = -1, msg = u'报名事件（%s）不存在' % name)
+
+        eventObj.content_fields = map(lambda x: form.FieldDescription(**x),
+                json.loads(request.form['content_fields']))
+        eventObj.start_time = form.datetime(request.form['start_time'])
+        eventObj.end_time = form.datetime(request.form['end_time'])
+        eventObj.save()
+        return jsonify(err_code = 0, msg = u'修改保存成功')
+
+    else:
+        args = { 'name': request.form['name'], 
+                'content_fields': map(lambda x: form.FieldDescription(**x),
+                    json.loads(request.form['content_fields'])),
+                'start_time': form.datetime(request.form['start_time']),
+                'end_time': form.datetime(request.form['end_time'])}
+        eventObj = form.Event(**args)
+
+        try:
+            eventObj.save()
+        except form.NameExisted:
+            return jsonify(err_code = -1, msg = u'此报名事件已存在')
+        except form.MySQLdb.IntegrityError:
+            return jsonify(err_code = -2, msg = u'数据库错误')
+
+        return jsonify(err_code = 0, msg = u'新报名事件创建成功 id：%d' % eventObj.id)
 
 @app.route('/admin/forms/<name>/query')
 def admin_forms_query(name):
