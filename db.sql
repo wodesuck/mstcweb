99249a6d922ee58e5c5b9569c0538134d1a6a351
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2013-08-29 17:21:02 +0800

    `users` table in db.sql

--- db.sql
+++ db.sql
@@ -33,3 +33,14 @@ CREATE TABLE `forms_data` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8;
 CREATE INDEX `event_id` ON `forms_data` (`event_id`);
+
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(32) NOT NULL,
+    `pwhash` VARCHAR(60) NOT NULL,
+    `salt` VARCHAR(29) NOT NULL,
+    `token` VARCHAR(32) DEFAULT NULL,
+    `client_feature` VARCHAR(32) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+CREATE UNIQUE INDEX `username` ON `users` (`username`);
