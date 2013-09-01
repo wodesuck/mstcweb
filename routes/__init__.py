ee6aa7d5654ec78e10d3a997c0332b7352085e98
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2013-09-01 13:21:05 +0800

    before_request and teardown

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from flask import Flask, g
+import MySQLdb
+from common.db import connect_db
+
 app = Flask(__name__)
+
+@app.before_request
+def init():
+    g.conn = connect_db()
+    g.cursor = g.conn.cursor()
+
+@app.teardown_request
+def teardown():
+    if hasattr(g, 'conn'):
+        g.conn.close()
 
 import routes.pages
 import routes.forms
