8740c92111e1a8ea006d314e4378c69edec12226
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2013-09-02 15:32:25 +0800

    do not wrap the auth funcs into User class any more

--- common/userauth.py
+++ common/userauth.py
@@ -4,110 +4,100 @@ import uuid
 from flask import session, request, jsonify, abort, g
 from common.db import connect_db
 
-__all__ = ['User']
+__all__ = ['login', 'logout', 'get_salt', 'check_auth', 'change_password']
 
-class User(object):
+def login(username, pwhash):
+    if 'SESSION_SALT' not in session:
+        abort(400)
 
-    def __init__(self, username, pwhash):
-        self.username = username
-        self.pwhash = pwhash
+    if not g.cursor.execute(
+            "SELECT `pwhash` FROM `users` WHERE `username` = %s",
+            username):
+        abort(403)
 
-    def login(self):
-        if 'SESSION_SALT' not in session:
-            abort(400)
+    if pwhash != bcrypt.hashpw(
+            g.cursor.fetchone()[0], session['SESSION_SALT']):
+        abort(403)
 
-        if not g.cursor.execute(
-                "SELECT `pwhash` FROM `users` WHERE `username` = %s",
-                self.username):
-            abort(403)
+    token = uuid.uuid4().hex
+    g.cursor.execute(
+            """UPDATE `users` SET `token` = %s, `client_feature` = %s
+            WHERE `username` = %s""",
+            (token, _get_client_feature(), username))
+    g.conn.commit()
 
-        if self.pwhash != bcrypt.hashpw(
-                g.cursor.fetchone()[0], session['SESSION_SALT']):
-            abort(403)
+    session['USERNAME'] = username
+    session['TOKEN'] = token
+    session.pop('SESSION_SALT')
 
-        token = uuid.uuid4().hex
+    return True
+
+def change_password(old_pwhash, new_pwhash):
+    if 'SESSION_SALT' not in session:
+        abort(400)
+
+    if not check_auth():
+        abort(403)
+
+    g.cursor.execute(
+            "SELECT `pwhash` FROM `users` WHERE `username` = %s",
+            session['USERNAME'])
+    if g.cursor.fetchone()[0] != old_pwhash:
+        abort(403)
+
+    g.cursor.execute(
+            """UPDATE `users` SET `salt` = %s, `pwhash` = %s, `token` = %s,
+            WHERE `username` = %s""",
+            (session['SESSION_SALT'], new_pwhash, uuid.uuid4().hex,
+            session['USERNAME']))
+    g.conn.commit()
+
+    session.pop('TOKEN')
+    session.pop('SESSION_SALT')
+    return True
+
+def logout():
+    if check_auth():
         g.cursor.execute(
-                """UPDATE `users` SET `token` = %s, `client_feature` = %s
-                WHERE `username` = %s""",
-                (token, self._get_client_feature(), self.username))
-        g.conn.commit()
-
-        session['USERNAME'] = self.username
-        session['TOKEN'] = token
-        session.pop('SESSION_SALT')
-
-        return True
-
-    def change_password(self, old_pwhash, new_pwhash):
-        if 'SESSION_SALT' not in session:
-            abort(400)
-
-        if not self.check_auth():
-            abort(403)
-
-        g.cursor.execute(
-                "SELECT `pwhash` FROM `users` WHERE `username` = %s",
-                session['USERNAME'])
-        if g.cursor.fetchone()[0] != old_pwhash:
-            abort(403)
-
-        g.cursor.execute(
-                """UPDATE `users` SET `salt` = %s, `pwhash` = %s, `token` = %s,
-                WHERE `username` = %s""",
-                (session['SESSION_SALT'], new_pwhash, uuid.uuid4().hex,
-                session['USERNAME']))
+                "UPDATE `users` SET `token` = %s WHERE `username` = %s",
+                (uuid.uuid4().hex, session['USERNAME']))
         g.conn.commit()
 
         session.pop('TOKEN')
-        session.pop('SESSION_SALT')
         return True
+    else:
+        return False
 
-    @classmethod
-    def logout(cls):
-        if cls.check_auth():
-            g.cursor.execute(
-                    "UPDATE `users` SET `token` = %s WHERE `username` = %s",
-                    (uuid.uuid4().hex, session['USERNAME']))
-            g.conn.commit()
+def check_auth():
+    if not ('USERNAME' in session and
+            'TOKEN' in session):
+        return False
 
-            session.pop('TOKEN')
-            return True
-        else:
-            return False
+    if not g.cursor.execute(
+        """SELECT `token`, `client_feature`
+        FROM `users` WHERE `username` = %s""",
+        session['USERNAME']):
+        return False
+    token, client_feature = g.cursor.fetchone()
 
-    @classmethod
-    def check_auth(cls):
-        if not ('USERNAME' in session and
-                'TOKEN' in session):
-            return False
+    return (token == session['TOKEN'] and
+            client_feature == _get_client_feature())
 
-        if not g.cursor.execute(
-            """SELECT `token`, `client_feature`
-            FROM `users` WHERE `username` = %s""",
-            session['USERNAME']):
-            return False
-        token, client_feature = g.cursor.fetchone()
+def _get_client_feature():
+    m = hashlib.md5()
+    m.update(request.user_agent.string)
+    if request.remote_addr is not None:
+        m.update(request.remote_addr)
+    return m.hexdigest()
 
-        return (token == session['TOKEN'] and
-                client_feature == cls._get_client_feature())
+def get_salt(username):
+    if not g.cursor.execute(
+            "SELECT `salt` FROM `users` WHERE `username` = %s", username):
+        abort(404)
+    account_salt = g.cursor.fetchone()[0]
 
-    @staticmethod
-    def _get_client_feature():
-        m = hashlib.md5()
-        m.update(request.user_agent.string)
-        if request.remote_addr is not None:
-            m.update(request.remote_addr)
-        return m.hexdigest()
+    session['SESSION_SALT'] = bcrypt.gensalt(8)
 
-    @staticmethod
-    def get_salt(username):
-        if not g.cursor.execute(
-                "SELECT `salt` FROM `users` WHERE `username` = %s", username):
-            abort(404)
-        account_salt = g.cursor.fetchone()[0]
-
-        session['SESSION_SALT'] = bcrypt.gensalt(8)
-
-        return {'account_salt': account_salt,
-                'session_salt': session['SESSION_SALT']} 
+    return {'account_salt': account_salt,
+            'session_salt': session['SESSION_SALT']} 
 
--- routes/userauth.py
+++ routes/userauth.py
@@ -1,27 +1,26 @@
 from routes import app
-from common.userauth import User
+from common import userauth
 from flask import request, jsonify
 
 @app.route('/admin/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        salt = User.get_salt(request.args['username'])
+        salt = userauth.get_salt(request.args['username'])
         salt['err_code'] = 0
         return jsonify(**salt)
     else:
-        userObj = User(request.form['username'], request.form['pwhash'])
-        if userObj.login():
+        if userauth.login(request.form['username'], request.form['pwhash']):
             return jsonify(err_code = 0)
         else:
             return jsonify(err_code = -1)
 
 @app.route('/admin/logout')
 def logout():
-    if User.logout():
+    if userauth.logout():
         return jsonify(err_code = 0)
     else:
         return jsonify(err_code = -1)
 
 @app.route('/admin/login/test')
 def login_test():
-    return str(int(User.check_auth()))
+    return str(int(userauth.check_auth()))
