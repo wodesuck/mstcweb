5a1ba5d2824ae3f035b9c4b2a95d010a18ed4dae
wodesuck <wodesuck@gmail.com>
2013-09-05 19:34:09 +0800

    add route GET /pages/<name>

--- routes/pages.py
+++ routes/pages.py
@@ -1,7 +1,17 @@
+# -*- coding: utf8 -*-
 from routes import app
-from models import page
+from models.page import Page, NoSuchPage
+from flask import render_template, abort
+from jinja2.exceptions import TemplateNotFound
 
 
 @app.route('/pages/<name>')
 def show_page(name):
-    return 'Hello %s!' % name
+    try:
+        page = Page.get(name)
+        return render_template('layouts/%s.html' % page.layout,
+                               **page.__dict__)
+    except NoSuchPage:
+        abort(404)
+    except TemplateNotFound:
+        abort(404)
