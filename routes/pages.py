f2b69b155a2aa1602366bbdcb0218bdb4e38a7ed
wodesuck <wodesuck@gmail.com>
2013-09-07 10:29:03 +0800

    change routes

--- routes/pages.py
+++ routes/pages.py
@@ -19,60 +19,28 @@ def show_page(name):
         abort(404)
 
 
-@app.route('/admin/pages/new')
-def admin_pages_new(name):
-    if not check_auth():
-        abort(403)
-
-    return render_template('page_new.html')
-
-
-@app.route('/admin/pages/<name>', methods=['DELETE'])
-def admin_pages_delete(name):
-    if not check_auth():
-        abort(403)
-
-    Page.delete(name)
-    return jsonify(err_code=0, msg=u'页面（%s）已删除' % name)
-
-
-@app.route('/admin/pages/<name>/edit')
-def admin_pages_edit(name):
-    if not check_auth():
-        abort(403)
-
-    try:
-        page = Page.get(name)
-        return render_template('page_edit.html', **page.__dict__)
-    except NoSuchPage:
-        abort(404)
-
-
-@app.route('/admin/pages', methods=['PATCH', 'POST'])
-def admin_pages_save():
+@app.route('/admin/pages/new', methods=['GET', 'POST'])
+def admin_pages_new():
     """
-    accept requests from /admin/pages/new or /admin/pages/<name>/edit
-    save changes to database
-    return msg:
-        -1 -- patched page doesn't exist / same name has existed
-        -2 -- database error
-        0 -- success
-    POST: from /admin/pages/new, insert a new page to database
-    PATCH: from /admin/pages/<name>/edit, change an existing entry in database
+    Create a new page.
+    Administrator should have logged in to access this page.
+
+    GET: Show the 'create page' page.
+
+    POST: Save the new page. Return one of the following messages.
+
+        same name exist (err_code = -1)
+        database error  (err_code = -2)
+        success         (err_code =  0)
     """
     if not check_auth():
         abort(403)
 
-    name = request.form['name']
-    if request.method == 'PATCH':
-        try:
-            Page.get(name).update(**request.form)
-        except NoSuchPage:
-            return jsonify(err_code=-1, msg=u'页面（%s）不存在' % name)
-
-        return jsonify(err_code=0, msg=u'修改保存成功')
+    if request.method == 'GET':
+        return render_template('page_new.html')
     else:
         try:
+            name = request.form['name']
             Page.create(**request.form)
         except PageNameExist:
             return jsonify(err_code=-1, msg=u'页面名称（%s）已存在' % name)
@@ -80,3 +48,42 @@ def admin_pages_save():
             return jsonify(err_code=-1, msg=u'数据库错误')
 
         return jsonify(err_code=0, msg=u'页面新建成功')
+
+
+@app.route('/admin/pages/<name>/edit', methods=['GET', 'POST'])
+def admin_pages_edit(name):
+    """
+    Change an existing page.
+    Administrator should have logged in to access this page.
+    If argument 'name' is not an existing page name, abort 404
+
+    GET: Show the 'edit page' page.
+
+    POST: Save the change.
+    """
+    if not check_auth():
+        abort(403)
+
+    try:
+        page = Page.get(name)
+    except NoSuchPage:
+        abort(404)
+
+    if request.method == 'GET':
+        return render_template('page_edit.html', **page.__dict__)
+    else:
+        page.update(**request.form)
+        return jsonify(err_code=0, msg=u'修改保存成功')
+
+
+@app.route('/admin/pages/<name>/delete', methods=['POST'])
+def admin_pages_delete(name):
+    """
+    Delete the specific page.
+    Administrator should have logged in to access this page.
+    """
+    if not check_auth():
+        abort(403)
+
+    Page.delete(name)
+    return jsonify(err_code=0, msg=u'页面（%s）已删除' % name)
