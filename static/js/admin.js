35d21c6d612f5fa73a499c1ed3e58110275d0923
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2013-09-14 16:56:28 +0800

    finish login function

---
+++ static/js/admin.js
@@ -0,0 +1,29 @@
+function login() {
+  var username = $('#main-login-form input[type="text"]').val();
+  var password = $('#main-login-form input[type="password"]').val();
+  var hasher = new bCrypt();
+
+  $('#main-login-form .alert').hide(100);
+
+  $.get('/admin/login', { username: username }, function(result) {
+    hasher.hashpw(password, result.account_salt, function(pwhash1) {
+      hasher.hashpw(pwhash1, result.session_salt, function(pwhash2) {
+        $.post('/admin/login', { username: username, pwhash: pwhash2 }, function(result) {
+          if(result.err_code == 0)
+            location.reload();
+        }).fail(function() {
+          $('#main-login-form input[type="password"]').val('').focus();
+          $('#main-login-form .alert').show(200);
+        });
+      });
+    });
+  });
+}
+
+$(function() {
+  //Register events for login page
+  $('#main-login-form button').click(function(e) {
+    e.preventDefault();
+    login();
+  });
+});
